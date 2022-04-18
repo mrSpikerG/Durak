2c48c3f331278d47adb543fb10fa9c1388e781af
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2022-04-18 18:29:21 +0300

    Добавьте файлы проекта.

---
+++ Durak/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Durak
+{
+    class Card
+    {
+        public int NumValue { get; set; }
+        public string CharValue { get; set; }
+
+        public Card()
+        {
+
+        }
+    }
+}
---
+++ Durak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Durak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
